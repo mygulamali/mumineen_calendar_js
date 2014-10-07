b6d576b8b61843c09b6d118a8027e3d54961c04b
Murtaza Gulamali <mygulamali@gmail.com>
2014-10-07 11:26:47 +0100

    Move assets into dedicated directory.

--- config.rb
+++ config.rb
@@ -47,11 +47,9 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :css_dir, 'assets/stylesheets'
+set :images_dir, 'assets/images'
+set :js_dir, 'assets/javascripts'
 
 # Build-specific configuration
 configure :build do
@@ -65,7 +63,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
