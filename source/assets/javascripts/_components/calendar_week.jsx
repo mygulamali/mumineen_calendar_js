997eba755ec5bd5304c144720e94ecf1a25e14c6
Murtaza Gulamali <mygulamali@gmail.com>
2014-12-05 16:51:22 +0000

    Pass miqaat data down to CalendarDay components.

--- source/assets/javascripts/_components/calendar_week.jsx
+++ source/assets/javascripts/_components/calendar_week.jsx
@@ -2,11 +2,12 @@
 var CalendarWeek = React.createClass({
   days: function () {
     var today = this.props.today,
+        miqaats = this.props.miqaats,
         onDayClick = this.props.onDayClick;
     return Lazy(this.props.week).map(function (day) {
       var key = [day.hijri.year, day.hijri.month, day.hijri.date].join("-");
       return (
-        <CalendarDay key={key} day={day} today={today} onDayClick={onDayClick} />
+        <CalendarDay key={key} day={day} today={today} miqaats={miqaats} onDayClick={onDayClick} />
       );
     }).toArray();
   },
