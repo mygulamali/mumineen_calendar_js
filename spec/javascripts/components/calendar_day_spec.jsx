757ebb22915b0cb95e65f23e9b6232a26487376f
Murtaza Gulamali <mygulamali@gmail.com>
2014-12-06 11:41:07 +0000

    Add specs for CalendarDay component.

--- spec/javascripts/components/calendar_day_spec.jsx
+++ spec/javascripts/components/calendar_day_spec.jsx
@@ -99,5 +99,174 @@
         });
       });
     });
+
+    describe("iconClassName", function () {
+      var key,
+          day,
+          today,
+          miqaats,
+          instance;
+
+      beforeEach(function () {
+        today = new HijriDate(1432, 3, 20);
+        miqaats = [
+          {
+            "month": 0,
+            "date": 1,
+            "miqaats": [
+              {
+                "title": "Miqaat 1",
+                "description": null,
+                "phase": "day",
+                "priority": 1,
+                "year": 1436
+              }
+            ]
+          },
+          {
+            "month": 0,
+            "date": 2,
+            "miqaats": [
+              {
+                "title": "Miqaat 2",
+                "description": null,
+                "phase": "night",
+                "priority": 1,
+                "year": 1436
+              },
+              {
+                "title": "Miqaat 3",
+                "description": "A description",
+                "phase": "day",
+                "priority": 3,
+                "year": null
+              }
+            ]
+          },
+          {
+            "month": 0,
+            "date": 3,
+            "miqaats": [
+              {
+                "title": "Miqaat 4",
+                "description": "Another description",
+                "phase": "day",
+                "priority": 3,
+                "year": null
+              }
+            ]
+          }
+        ]
+      });
+
+      afterEach(function () {
+        if (instance && instance.isMounted()) {
+          instance.unmountComponent();
+        }
+      });
+
+      describe("when the calendar day is a filler day", function () {
+        beforeEach(function () {
+          key = "1436-0-1";
+          day = createDayObject(new HijriDate(1436, 0, 1));
+          day.filler = true;
+          instance = TestUtils.renderIntoDocument(
+            <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+          );
+        });
+
+        it("expects to return null", function () {
+          expect(instance.iconClassName()).toEqual(null);
+        });
+      });
+
+      describe("when there are no miqaats for the calendar day", function () {
+        beforeEach(function () {
+          key = "1436-0-4";
+          day = createDayObject(new HijriDate(1436, 0, 4));
+          instance = TestUtils.renderIntoDocument(
+            <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+          );
+        });
+
+        it("expects to return null", function () {
+          expect(instance.iconClassName()).toEqual(null);
+        });
+      });
+
+      describe("when there are miqaats for the calendar day", function () {
+        describe("when the first miqaat is of priority 1 and takes place in the day", function () {
+          beforeEach(function () {
+            key = "1436-0-1";
+            day = createDayObject(new HijriDate(1436, 0, 1));
+            instance = TestUtils.renderIntoDocument(
+              <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+            );
+          });
+
+          it("expects to return the classname for the Sun icon", function () {
+            expect(instance.iconClassName()).toEqual("icon-sun");
+          });
+        });
+
+        describe("when the first miqaat is of priority 1 and takes place in the night", function () {
+          beforeEach(function () {
+            key = "1436-0-2";
+            day = createDayObject(new HijriDate(1436, 0, 2));
+            instance = TestUtils.renderIntoDocument(
+              <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+            );
+          });
+
+          it("expects to return the classname for the Moon icon", function () {
+            expect(instance.iconClassName()).toEqual("icon-moon");
+          });
+        });
+
+        describe("when the first miqaat is a priority greater than 1", function () {
+          beforeEach(function () {
+            key = "1436-0-3";
+            day = createDayObject(new HijriDate(1436, 0, 3));
+            instance = TestUtils.renderIntoDocument(
+              <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+            );
+          });
+
+          it("expects to return the classname for the dot icon", function () {
+            expect(instance.iconClassName()).toEqual("icon-circle");
+          });
+        });
+
+        describe("when the year of the first miqaat is after the year of the calendar day", function () {
+          describe("when there are no other valid miqaats on the calendar day", function () {
+            beforeEach(function () {
+              key = "1435-0-1";
+              day = createDayObject(new HijriDate(1435, 0, 1));
+              instance = TestUtils.renderIntoDocument(
+                <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+              );
+            });
+
+            it("expects to return null", function () {
+              expect(instance.iconClassName()).toEqual(null);
+            });
+          });
+
+          describe("when there is another valid miqaat on the calendar day", function () {
+            beforeEach(function () {
+              key = "1435-0-2";
+              day = createDayObject(new HijriDate(1435, 0, 2));
+              instance = TestUtils.renderIntoDocument(
+                <CalendarDay key={key} day={day} today={today} miqaats={miqaats} />
+              );
+            });
+
+            it("expects to return the classname for the next valid miqaat", function () {
+              expect(instance.iconClassName()).toEqual("icon-circle");
+            });
+          });
+        });
+      });
+    });
   });
 })();
