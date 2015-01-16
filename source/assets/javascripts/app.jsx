aae05e5e65b0d0ff54dc89f8db8c22b69fbc6db4
Murtaza Gulamali <mygulamali@gmail.com>
2015-01-16 12:42:39 +0000

    Update method name.

--- source/assets/javascripts/app.jsx
+++ source/assets/javascripts/app.jsx
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 if (document.getElementsByTagName('main').length > 0) {
-  React.renderComponent(
+  React.render(
     <CalendarFrame today={HijriDate.fromGregorian(new Date())} />,
     document.getElementsByTagName('main').item(0)
   );
